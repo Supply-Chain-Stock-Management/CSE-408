5ba763589f90d8bbcc5672e020e50234cb0bf3a1
jarif <jariftc001@gmail.com>
2024-02-17 19:24:52 +0600

    addShipPage

---
+++ js/shipment.js
@@ -0,0 +1,14 @@
+function showPOCard() {  
+    var cardPart = document.getElementById('addCard');
+    cardPart.style.cssText += 'visibility: visible';
+    var mainPage = document.getElementById('mainPage');
+    mainPage.style.cssText += 'opacity: 0.3';
+}  
+
+function resetNow(){
+    var cardPart = document.getElementById('addCard');
+    cardPart.style.cssText += 'visibility: hidden';
+    var mainPage = document.getElementById('mainPage');
+    mainPage.style.cssText += 'opacity: 1';
+}
+  
